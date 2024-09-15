2cdc34149a22c6d547ba1de75f5bf6705334ad87
azcpavel <azc.pavel@gmail.com>
2024-09-15 13:07:42 +0600

    Dto Update

--- src/Http/Dtos/CreditInformationDto.php
+++ src/Http/Dtos/CreditInformationDto.php
@@ -54,8 +54,8 @@ class CreditInformationDto {
 
     private function setCrAccountOrChallanNo(string $crAccountOrChallanNo): void {        
         // Check length limit if provided
-        if ($crAccountOrChallanNo !== null && strlen($crAccountOrChallanNo) > 26) {
-            throw new InvalidArgumentException('CrAccountOrChallanNo must be less than or equal to 26 characters.');
+        if ($crAccountOrChallanNo !== null && (strlen($crAccountOrChallanNo) < 13 || strlen($crAccountOrChallanNo) > 26)) {
+            throw new InvalidArgumentException('CrAccountOrChallanNo must be greater than or 13 and less than or equal to 26 characters.');
         }
         $this->crAccountOrChallanNo = $crAccountOrChallanNo;
     }
