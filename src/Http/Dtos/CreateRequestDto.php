4cdbfe9982d8ecdb28ab79cc8b42b8e25bb63d78
azcpavel <azc.pavel@gmail.com>
2025-04-07 17:49:54 +0600

    Phone number limit change

--- src/Http/Dtos/CreateRequestDto.php
+++ src/Http/Dtos/CreateRequestDto.php
@@ -110,8 +110,8 @@ class CreateRequestDto {
         // Validate format based on Bangladeshi or international numbers
         // For simplicity, assuming it's a general validation
         $filteredString = preg_replace("/[^0-9]/", "", $customerContactNo);
-        if (strlen($filteredString) < 11 || strlen($filteredString) > 15) {
-            throw new InvalidArgumentException('CustomerContactNo must be greater than or equal to 11 less than or equal to 15 characters.');
+        if (strlen($filteredString) < 7 || strlen($filteredString) > 15) {
+            throw new InvalidArgumentException('CustomerContactNo must be greater than or equal to 7 less than or equal to 15 characters.');
         }
         $this->customerContactNo = $filteredString;
     }
